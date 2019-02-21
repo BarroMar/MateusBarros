9cf109a67c0c52e9d1cd42c215201eb32dc6ba9b
BarroMar <mateus.a98@outlook.com>
2019-02-21 20:02:25 -0300

    operação switch case

--- src/Exemplo/Catalogo.java
+++ src/Exemplo/Catalogo.java
@@ -8,11 +8,28 @@ public class Catalogo {
             a = c;
         }
         System.out.println(a);
+        //continuação1
         if (c>a) {
             c = a;
         }else{
             c = b;
         }
         System.out.println(c);
+        //switch
+        int op =3;
+        int v = 0;
+        switch (op) {
+            case 1:
+                v = 1;
+                break;
+
+            case 2:
+                v = 2;
+                break;
+            default:
+                v = -1;
+        }
+        System.out.println( v );
+        }
     }
 }
