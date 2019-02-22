a69fcd58aa54809c4ada818839126097102cd5f1
BarroMar <mateus.a98@outlook.com>
2019-02-21 21:05:03 -0300

    exercicios

--- src/Exercicios_Revisão/Ex01.java
+++ src/Exercicios_Revisão/Ex01.java
@@ -1,7 +1,20 @@
 package Exercicios_Revisão;
 
+import java.util.Scanner;
+
 public class Ex01 {
     public static void main(String[] args){
+        int totalfolhas;
+        double resultado;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("informe o numero de folhas a serem impressas");
+        totalfolhas = sc.nextInt();
+        if (totalfolhas <= 100){
+            resultado = totalfolhas * 0.25;
+        }else{
+            resultado = totalfolhas * 0.20;
+        }
+        System.out.println(resultado);
 
     }
 }
